6ba5eb220dbe670996357508706ee1b63865ad5b
A Zhila <aleksej.zhila@gmail.com>
2020-02-08 18:49:09 +0300

    Добавляет массивы с данными и функцию рандом

--- js/setup.js
+++ js/setup.js
@@ -1,2 +1,14 @@
 'use strict';
 
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var setupWindow = document.querySelector('.setup');
+setupWindow.classList.remove('hidden');
+
+var getRandom = function (array) {
+  return Math.floor(Math.random() * array.length);
+};
+
