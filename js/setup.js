9ed612dfd9498522fe1a75a109a5fc8405be141a
A Zhila <aleksej.zhila@gmail.com>
2020-02-17 21:03:18 +0300

    Добавляет управление отображением попапа

--- js/setup.js
+++ js/setup.js
@@ -2,15 +2,57 @@
 // количество волшебников
 var NUMBER_OF_WIZARDS = 4;
 
+// кнопки для обработчиков
+
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
+
 // массивы с данными
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-// показываем модалку
+// показываем попап
 var setupWindow = document.querySelector('.setup');
-setupWindow.classList.remove('hidden');
+var setupOpenIcon = document.querySelector('.setup-open-icon');
+var setupCloseButton = setupWindow.querySelector('.setup-close');
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    setupClose();
+  }
+};
+
+var setupOpen = function () {
+  setupWindow.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var setupClose = function () {
+  setupWindow.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpenIcon.addEventListener('click', function () {
+  setupOpen();
+});
+
+setupOpenIcon.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    setupOpen();
+  }
+});
+
+setupCloseButton.addEventListener('click', function () {
+  setupClose();
+});
+
+setupCloseButton.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    setupClose();
+  }
+});
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
